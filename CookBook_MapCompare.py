dd3047ce554f8b9ed1b0c9551459a731c2ada591
Eischias <wset@seznam.cz>
2022-06-28 11:17:05 +0200

    update

--- CookBook_MapCompare.py
+++ CookBook_MapCompare.py
@@ -1,1 +1,21 @@
+import numpy
+import pandas as pd
 
+# load data from csv
+dat = list(pd.read_csv("result.csv",";").itertuples(index=False))
+imgSize = (3840,2160)
+
+test = draw_heatmap(dat,imgSize)
+
+where_are_NaNs = (test)
+test[where_are_NaNs] = 0
+
+numpy.nanmin(test)
+
+test[test <= 1000] = "nan"
+
+
+
+fig, ax = draw_display(imgSize, imagefile=None)
+ax.imshow(test, cmap='jet', alpha=0.5)
+ax.invert_yaxis()
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Saliency_map_Vs_gaz_emap
 evaluate difference between gaze map and saliency prediction
+
+1) run all predefined function from "function.py"
+(function are based from these project https://github.com/TobiasRoeddiger/GazePointHeatMap)
+2) 
